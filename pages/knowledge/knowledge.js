e2bee1a93e2a636f62d48a3f1df10c28a1e7922f
liutao <tao.liu@uurbot.cn>
2019-12-11 18:19:39 +0800

    增加知识体系首页

--- pages/knowledge/knowledge.js
+++ pages/knowledge/knowledge.js
@@ -5,62 +5,78 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    kdListDatas: [],
+    loading: false,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    var that = this
+    this.setData({
+      loading: true
+    })
+    wx.request({
+      url: 'https://www.wanandroid.com/tree/json',
+      success(res) {
+          console.log("请求结果：",res.data)
+          if(res.data.errorCode == 0){
+              that.setData({
+                kdListDatas:res.data.data,
+                loading:false
+              })
+          }
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
